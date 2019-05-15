ad940c17eb769f8ba8c9c28eb2bea8d9489579e1
xiejianxin520 <1411778970@qq.com>
2019-05-15 17:26:25 +0800

    分页效果完成

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -21,6 +21,9 @@
       <el-table-column prop="address" label="操作">
       </el-table-column>
     </el-table>
+    <!-- //分页    layout 分页显示的内容 给 current-page 属性添加 .sync 修饰符后, 就可以设置当前页-->
+    <el-pagination background layout="prev, pager, next" :total="total" :pageSize='pageSize' :current-page="curPage" @current-change='currentChange'>
+    </el-pagination>
   </div>
 </template>
 
@@ -29,31 +32,41 @@ export default {
   name: 'User',
   data() {
     return {
-      userlist: []
+      userlist: [],
+      pageSize: 3, // 每页条数
+      total: 10, // 总条数
+      curPage: 3 // 当前页码
     }
   },
   created() {
     this.getUserList()
   },
   methods: {
-    getUserList() {
+    getUserList(curPage = 1) {
       this.$http
         .get('/users', {
           params: {
-            pagenum: 1, // 当前页
+            pagenum: curPage, // 当前页
             pagesize: 3 // 每页展示多少条数据
           }
           // headers: { Authorization: localStorage.getItem('token') } // 将 token 作为请求头，传递给服务器接口这样，才能正确的调用这个接口
         })
         .then(res => {
-          console.log('USER返回数据', res)
+          console.log('USER返回页码数据', res) //里面有包含总页数
           // const { data, meta } = res.data
           const data = res.data.data
           const meta = res.data.meta
           if (meta.status === 200) {
             this.userlist = data.users
+            this.total = data.total
           }
         })
+    },
+    //------跳转到想要分页数据.*********
+    currentChange(a) {
+      // 就把当前页数参数传进去获取
+      // console.log(a)
+      this.getUserList(a)
     }
   },
   components: {}
